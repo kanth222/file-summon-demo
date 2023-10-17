0e33ef75b6ab611b8b0d0807c1eaad61fde6cbf4
unknown <LocalAccount@cybappserver.atlanta.testing-labs.net>
2023-10-17 07:45:02 -0400

    updated db.config.js,server.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: process.env.DB_HOST || "172.16.11.53",
-  USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "demo-db",
-  DB: process.env.DB_NAME || "nodedemo",
+  HOST: process.env["DB_HOST"],
+  USER: process.env["DB_USER"],
+  PASSWORD: process.env["DB_PASSWORD"],
+  DB: process.env["DB_NAME"],
   PORT: process.env["DB_PORT"]
 };
